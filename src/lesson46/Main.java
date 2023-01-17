c6cbc7a705285d8dbda352e3f12694c2dd971a4f
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2023-01-17 12:44:44 +0100

    refactor: change ArrayList to List & remove some debug print lines

--- src/lesson46/Main.java
+++ src/lesson46/Main.java
@@ -1,20 +1,21 @@
 package lesson46;
 
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> simpleResult = addToArray(new ArrayList<>(), 100000);
-        ArrayList<Integer> rndResult = addRndToArray(new ArrayList<>(), 100000);
+        List<Integer> simpleResult = addToArray(new ArrayList<>(), 100000);
+        List<Integer> rndResult = addRndToArray(new ArrayList<>(), 100000);
 
         printResult(simpleResult, 0, 16);
         printResult(rndResult, 0, 12);
     }
 
-    static ArrayList<Integer> addToArray(ArrayList<Integer> initList, int size) {
+    static List<Integer> addToArray(List<Integer> initList, int size) {
 
-        ArrayList<Integer> addList = new ArrayList<>(size);
+        List<Integer> addList = new ArrayList<>(size);
         for (int i = 0; i < size;  i++) {
             addList.add(i);
         }
@@ -22,18 +23,15 @@ public class Main {
         return initList;
     }
 
-    static ArrayList<Integer> addRndToArray(ArrayList<Integer> initList, int size) {
+    static List<Integer> addRndToArray(List<Integer> initList, int size) {
         Random rnd = new Random();
-        ArrayList<Integer> sourceList = new ArrayList<>(size);
-        ArrayList<Integer> addList = new ArrayList<>(size);
+        List<Integer> sourceList = new ArrayList<>(size);
+        List<Integer> addList = new ArrayList<>(size);
 
         for (int i = 0; i < size;  i++) {
             sourceList.add(i);
-            addList.add(-1);
+            addList.add(null);
         }
-
-        System.out.println("Source size " + sourceList.size());
-        System.out.println("Add list size " + addList.size());
 
         for (int i = 0; i < size;  i++) {
             int rndIdx = rnd.nextInt(size - i);
@@ -45,8 +43,11 @@ public class Main {
         return initList;
     }
 
-    static void printResult(ArrayList<Integer> result, int fromIdx, int toIdx) {
+    static void printResult(List<Integer> result, int fromIdx, int toIdx) {
         System.out.println("\nSize of final array: " + result.size());
+        System.out.println("List of elements form " + fromIdx + " to " + toIdx);
+        System.out.println("—————===***===—————");
+
         for (int i = fromIdx; i < toIdx; i++) {
             System.out.println(result.get(i));
         }
